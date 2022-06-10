faca786b223275f11934faa820cf97b381e4d227
Andre Ferreira <andre.dev.linux@gmail.com>
2022-06-10 17:15:13 -0300

    Adicionado @Transactional para metodos de persistencia em base de dados

--- src/main/java/br/dev/multicode/mcproducts/services/impl/ProductServiceImpl.java
+++ src/main/java/br/dev/multicode/mcproducts/services/impl/ProductServiceImpl.java
@@ -7,8 +7,8 @@ import br.dev.multicode.mcproducts.entities.Product;
 import br.dev.multicode.mcproducts.repositories.ProductRepository;
 import br.dev.multicode.mcproducts.services.ProductService;
 import lombok.RequiredArgsConstructor;
-import org.apache.commons.lang3.NotImplementedException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.UUID;
 
@@ -19,35 +19,46 @@ public class ProductServiceImpl implements ProductService {
   private final ProductRepository repository;
 
   @Override
-  public ProductResponse getById(final UUID productId) {
+  @Transactional(readOnly = true)
+  public ProductResponse getById(final UUID productId)
+  {
     return this.getProductById(productId.toString()).toResponse();
   }
 
   @Override
-  public String create(final ProductRequest postProductRequest) {
+  @Transactional
+  public String create(final ProductRequest postProductRequest)
+  {
     return repository.save(Product.of(postProductRequest)).getId();
   }
 
   @Override
-  public void update(final UUID productId, final ProductRequest putProductRequest) {
+  @Transactional
+  public void update(final UUID productId, final ProductRequest putProductRequest)
+  {
     Product product = this.getProductById(productId.toString());
     product.fillWith(putProductRequest);
     repository.save(product);
   }
 
   @Override
-  public void updatePartialContent(final UUID productId, final PatchProductRequest patchProductRequest) {
+  @Transactional
+  public void updatePartialContent(final UUID productId, final PatchProductRequest patchProductRequest)
+  {
     Product product = this.getProductById(productId.toString());
     product.fillWith(patchProductRequest);
     repository.save(product);
   }
 
   @Override
-  public void delete(final UUID productId) {
+  @Transactional
+  public void delete(final UUID productId)
+  {
     repository.delete(this.getProductById(productId.toString()));
   }
 
-  private Product getProductById(final String productId) {
+  private Product getProductById(final String productId)
+  {
     return repository.findById(productId)
       .orElseThrow(() ->
         new IllegalArgumentException("Product not found by ID=".concat(productId)));
