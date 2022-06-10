9ab8f9416eee8ae17b0c77752f5704e4bf5e4dd9
Andre Ferreira <andre.dev.linux@gmail.com>
2022-06-10 14:28:11 -0300

    Initial commit

---
+++ src/main/java/br/dev/multicode/mcproducts/McProductsApplication.java
@@ -0,0 +1,13 @@
+package br.dev.multicode.mcproducts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McProductsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(McProductsApplication.class, args);
+	}
+
+}
