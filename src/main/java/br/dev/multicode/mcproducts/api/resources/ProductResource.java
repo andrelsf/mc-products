280fc554ea33a8aa4a0147222dc4c6dc7c86b29f
Andre Ferreira <andre.dev.linux@gmail.com>
2022-06-10 17:14:26 -0300

    Adicionado recurso de produtos

---
+++ src/main/java/br/dev/multicode/mcproducts/api/resources/ProductResource.java
@@ -0,0 +1,63 @@
+package br.dev.multicode.mcproducts.api.resources;
+
+import br.dev.multicode.mcproducts.api.http.requests.PatchProductRequest;
+import br.dev.multicode.mcproducts.api.http.requests.ProductRequest;
+import br.dev.multicode.mcproducts.api.http.responses.ProductResponse;
+import br.dev.multicode.mcproducts.services.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import javax.validation.Valid;
+import java.net.URI;
+import java.util.UUID;
+
+@RestController
+@RequestMapping(
+  name = "/products",
+  produces = MediaType.APPLICATION_JSON_VALUE,
+  consumes = MediaType.APPLICATION_JSON_VALUE)
+@RequiredArgsConstructor
+public class ProductResource {
+
+  private final ProductService service;
+
+  @GetMapping("/{productId}")
+  public ResponseEntity<ProductResponse> getProductById(@PathVariable UUID productId)
+  {
+    return ResponseEntity.ok(service.getById(productId));
+  }
+
+  @PostMapping
+  public ResponseEntity<Void> postANewProduct(@RequestBody @Valid ProductRequest postProductRequest)
+  {
+    final String productId = service.create(postProductRequest);
+    final URI uriLocation = UriComponentsBuilder.fromUriString("/api/products/{productId}")
+      .buildAndExpand(productId)
+      .toUri();
+    return ResponseEntity.created(uriLocation).build();
+  }
+
+  @PutMapping("/{productId}")
+  public ResponseEntity<Void> putAProduct(@PathVariable UUID productId, @RequestBody @Valid ProductRequest putProductRequest)
+  {
+    service.update(productId, putProductRequest);
+    return ResponseEntity.noContent().build();
+  }
+
+  @PatchMapping("/{productId}")
+  public ResponseEntity<Void> pathAProduct(@PathVariable UUID productId, @RequestBody PatchProductRequest patchProductRequest)
+  {
+    service.updatePartialContent(productId, patchProductRequest);
+    return ResponseEntity.noContent().build();
+  }
+
+  @DeleteMapping("/{productId}")
+  public ResponseEntity<Void> deleteAProduct(@PathVariable UUID productId)
+  {
+    service.delete(productId);
+    return ResponseEntity.noContent().build();
+  }
+}
